5dd9b618c0215fbc8fd48f2b0fdb722603a34f90
Jade Allen Cook <jadeallencook@Jades-Macbook-Pro.local>
2018-05-31 21:24:34 -0600

    📱 Implemented more meta for mobile

--- photonichouse/waitlist/index.php
+++ photonichouse/waitlist/index.php
@@ -21,20 +21,19 @@
 </head>
   <body data-spy="scroll" data-target=".navbar" data-offset="71">
     <div class="wrapper">
-      <?php 
-        $sections = array(
-          'navbar',
-          'waitlist',
-          'footer',
-          'scroll-to-top',
-          'preloader',
-          'javascript'
-        ); 
-        foreach ($sections as $section) {
-            include_once("../components/$section.html");
-        }
-      ?>
+        <?php 
+          $sections = array(
+            'navbar',
+            'waitlist',
+            'footer',
+            'scroll-to-top',
+            'preloader',
+            'javascript'
+          ); 
+          foreach ($sections as $section) {
+              include_once("../components/$section.html");
+          }
+        ?>
     </div>
-</body>
-
+  </body>
 </html>
