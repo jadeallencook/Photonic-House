55c34fc20f28773ca2aeaa336d395827ce1eacda
Jerry Hansana <jerryhansana@JH0.local>
2018-06-10 01:38:00 -0600

    Added google analytics

--- photonichouse/index.php
+++ photonichouse/index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+ <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120648337-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120648337-1');
+</script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="refresh" content="0; URL='home'"/> 
-    
+
+<!-- favicon -->
+
 <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
